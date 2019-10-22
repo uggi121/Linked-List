7e495cbbf5bc0e0306d42c3fe91b45dbd0c110fc
uggi121 <sudharshanmadhavan@outlook.com>
2019-10-22 16:15:37 +0800

    Add size, is-empty

--- linked_list.py
+++ linked_list.py
@@ -24,8 +24,8 @@ class ListNode:
     Accepts a data value and a reference to the next node as inputs.
     
     Parameters:
-        data : Value to be stored in the node.
-        next_node (ListNode) : Reference to the next node.
+        data: Value to be stored in the node.
+        next_node (ListNode): Reference to the next node.
     """
     def __init__(self, data, next_node=None):
         self.data = data
@@ -41,16 +41,45 @@ class ListNode:
         return self.__unicode__()
 
 # Helper functions to process linked-lists.
+
 def search_list(L, key):
+    """
+    Searches a sequence of list-nodes for the presence of the given key.
+    
+    Parameters:
+        L (ListNode): The ListNode where the linear search commences.
+        key: The value to search for.
+        
+    Returns:
+        bool: True if 'key' is found, False otherwise.
+    """
     while L != None and L.data != key:
         L = L.next
     return L
 
+  
 def insert_after(node, new_node):
+    """
+    Inserts a new given ListNode 'new_node' after the current given
+    ListNode 'node'.
+    
+    Parameters:
+        node (ListNode): The node after which a new node is to be inserted.
+        new_node (ListNode): The node to be inserted.
+    """
+    assert node, "The node preceding the inserted node cannot be empty."
+    assert new_node, "The node to be inserted cannot be empty."
     new_node.next = node.next
     node.next = new_node
     
 def delete_after(node):
+    """
+    Deletes the ListNode following 'node'.
+    
+    Parameters:
+        node (ListNode): ListNode whose successor is to be deleted.
+    """
+    assert node, "Node cannot be empty."
     if node.next:
         node.next = node.next.next
         
@@ -58,12 +87,19 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.num_nodes = 0
         
     def get_head(self):
         return self.head
     
     def get_tail(self):
         return self.tail
+    
+    def is_empty(self):
+        return self.num_nodes == 0
+    
+    def size(self):
+        return self.num_nodes
     
     def add(self, item):
         node = ListNode(item)
@@ -72,6 +108,9 @@ class LinkedList:
         else:
             self.tail.next = node
             self.tail = self.tail.next
+            
+        # Increment list size.
+        self.num_nodes += 1
     
     def search(self, item):
         return search_list(self.head, item)
@@ -82,7 +121,10 @@ class LinkedList:
         if self.head.data == item:
             if self.head is self.tail:
                 self.tail = None
+                
             self.head = self.head.next
+            # Decrement list size.
+            self.num_nodes -= 1
             return True
         ptr = self.head
         while ptr.next and ptr.next.data != item:
@@ -92,6 +134,7 @@ class LinkedList:
             if self.tail is ptr.next:
                 self.tail = ptr
             delete_after(ptr)
+            self.num_nodes -= 1
             return True
         return False
     
