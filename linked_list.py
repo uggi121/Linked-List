afbeae1e6c8149802710f4cd6a3311fade234c86
uggi121 <sudharshanmadhavan@outlook.com>
2019-10-22 15:53:00 +0800

    First commit

---
+++ linked_list.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 15:49:05 2019
+
+@author: Sudharshan
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 14:44:07 2019
+
+@author: Sudharshan
+"""
+
+class ListNode:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+        
+    def __unicode__(self):
+        return "[" + str(self.data) + "]"
+    
+    def __str__(self):
+        return self.__unicode__()
+    
+    def __repr__(self):
+        return self.__unicode__()
+
+def search_list(L, key):
+    while L != None and L.data != key:
+        L = L.next
+    return True if L else False
+
+def insert_after(node, new_node):
+    new_node.next = node.next
+    node.next = new_node
+    
+def delete_after(node):
+    if node.next:
+        node.next = node.next.next
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def get_head(self):
+        return self.head
+    
+    def get_tail(self):
+        return self.tail
+    
+    def add(self, item):
+        node = ListNode(item)
+        if not self.head:
+            self.head, self.tail = node, node
+        else:
+            self.tail.next = node
+            self.tail = self.tail.next
+    
+    def search(self, item):
+        return search_list(self.head, item)
+    
+    def delete(self, item):
+        if not self.head:
+            raise ValueError("Cannot delete from empty list.")
+        if self.head.data == item:
+            if self.head is self.tail:
+                self.tail = None
+            self.head = self.head.next
+            return True
+        ptr = self.head
+        while ptr.next and ptr.next.data != item:
+            ptr = ptr.next
+        
+        if ptr.next:
+            if self.tail is ptr.next:
+                self.tail = ptr
+            delete_after(ptr)
+            return True
+        return False
+    
+    def __str__(self):
+        return str(self.head) if self.head else "Empty List"
+    
+    def __unicode__(self):
+        return self.__str__()
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def stringify(self):
+        ptr, res = self.head, []
+        while ptr:
+            res.append(ptr.data)
+            ptr = ptr.next
+        return str(res)
+            
+            
+    
+    
